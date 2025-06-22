938a9b3323f07c8031e724ab199cd24057ae8aa7
Mayamohamed207 <mayamohamed364@gmail.com>
2025-06-23 00:12:45 +0300

    Updates

---
+++ precache-manifest.3a39bfd898cffa7333f1ee737c37c112.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f42e159550a66572b90b5306a4dd9c0",
+    "url": "/Recipe-Guide/index.html"
+  },
+  {
+    "revision": "c7d9479675037d4417ff",
+    "url": "/Recipe-Guide/static/css/main.f3d3e81b.chunk.css"
+  },
+  {
+    "revision": "096fb005adaf5c414869",
+    "url": "/Recipe-Guide/static/js/2.b447fece.chunk.js"
+  },
+  {
+    "revision": "c7d9479675037d4417ff",
+    "url": "/Recipe-Guide/static/js/main.a1952aaf.chunk.js"
+  },
+  {
+    "revision": "d6cf498f79df9afcaa52",
+    "url": "/Recipe-Guide/static/js/runtime~main.5c4f4cf9.js"
+  }
+]);
