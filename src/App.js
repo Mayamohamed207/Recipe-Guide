1132d0b9afb5d824496739f8e3fbc98f4ab1aaa8
Mayamohamed207 <mayamohamed364@gmail.com>
2025-07-16 00:02:36 +0300

    edits

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import RecipePage from "./pages/RecipePage";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/Recipe-Guide">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipe/:id" element={<RecipePage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
